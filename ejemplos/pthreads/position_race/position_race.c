df1573ea900d26ae03907365346f3384d97053a3
Jorge Ignacio Chavarria Herrera <jchavaherrera@gmail.com>
2019-09-10 11:45:41 -0600

    Ejercicio position_race

---
+++ ejemplos/pthreads/position_race/position_race.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+typedef struct
+{
+	size_t thread_counter;
+	size_t position;
+}shared_data_t;
+
+typedef struct
+{
+	size_t thread_num;
+	shared_data_t* shared_data;
+}private_data_t;
+
+int create_thread(shared_data_t* shared_data);
+void* run(void* data);
+
+int main (int argc, char* argv[])
+{
+	shared_data_t* shared_data = (shared_data_t*) calloc(1, sizeof(shared_data_t));
+	if (shared_data == NULL)
+		return (void)fprintf(stderr, "error: could not allocate shared memory\n"), 1;
+	
+	shared_data->thread_counter = sysconf(_SC_NPROCESSORS_ONLN);
+	shared_data->position = 1;
+	
+	if(argc >= 2)
+		shared_data->thread_counter = strtoull(argv[1], NULL, 10);	
+	
+	struct timespec start_time;
+	clock_gettime(CLOCK_MONOTONIC, &start_time);
+	
+	int error = create_thread(shared_data);
+	if (error)
+		return error;
+		
+	struct timespec finish_time;
+	clock_gettime(CLOCK_MONOTONIC, &finish_time);
+	printf("Hello world from main thread/n");
+	
+	double elapsed_seconds = finish_time.tv_sec - start_time.tv_sec
+		+ 1e-9 * (finish_time.tv_nsec - start_time.tv_nsec);
+	
+	printf("Hello execution time %.9lfs\n", elapsed_seconds);
+	free(shared_data);
+	return 0;	
+}
+
+int create_thread(shared_data_t* shared_data)
+{
+	pthread_t* threads = (pthread_t*) malloc(shared_data->thread_counter * sizeof(pthread_t));
+	if (threads == NULL)
+		return (void)fprintf(stderr, "error: could not allocate memory for %zu threads\n", shared_data->thread_counter), 1;
+	
+	private_data_t* private_data = (private_data_t*) calloc(shared_data->thread_counter, sizeof(private_data_t));
+	if(private_data == NULL)
+		return (void)fprintf(stderr, "error: could not allocate private memory for %zu threads\n", shared_data->thread_counter), 1;
+	
+	for(size_t index = 0; index < shared_data->thread_counter; ++index)
+	{
+		private_data[index].thread_num = index;
+		private_data[index].shared_data = shared_data;
+		pthread_create(&threads[index], NULL, run, &private_data[index]);
+	}
+	
+	for(size_t index = 0; index < shared_data->thread_counter; ++index)
+		pthread_join(threads[index], NULL);
+	
+	free(private_data);
+	free(threads);
+	
+	return 0;
+}
+
+void* run(void* data)
+{
+	private_data_t* private_data = (private_data_t *) data;
+	shared_data_t* shared_data = private_data->shared_data;
+	size_t thread_num = private_data->thread_num;
+	size_t thread_counter = shared_data->thread_counter; 
+	printf("Thread %zu/%zu: I arrived at position %zu\n", thread_num, thread_counter, shared_data->position);
+	shared_data->position++;
+	return NULL;
+}
