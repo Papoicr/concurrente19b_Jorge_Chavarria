345c62a051b41496948e53ff3cf42f6e05182049
Jorge Ignacio Chavarria Herrera <jchavaherrera@gmail.com>
2019-11-05 11:27:23 -0600

    several for listo

---
+++ ejemplos/OpenMP/several_for/several_for.cpp
@@ -0,0 +1,43 @@
+#include <cstdlib>
+#include <iostream>
+#include <omp.h>
+
+int main (int argc, char* argv[])
+{
+	int thread_count = omp_get_max_threads();
+	if(argc >= 2)
+		thread_count = atoi(argv[1]);
+		
+	int iteration_count = thread_count;
+	if(argc >= 2)
+		iteration_count = atoi(argv[2]);
+	
+	#pragma omp parallel num_threads(thread_count) \
+		default(none) shared(iteration_count, std::cout) /// El pragma es una instruccion para el compilador específico y los de openMP llevan omp
+	{
+		#pragma omp for
+		for (int iteration = 0; iteration < iteration_count; ++iteration)
+		{
+			#pragma omp critical(cout)
+			std::cout << "Stage 1 " << omp_get_thread_num () << " / " << omp_get_num_threads() << 
+				": iteration " << iteration << " / " << iteration_count << std::endl;
+		}
+		
+		#pragma omp for
+		for (int iteration = 0; iteration < iteration_count; ++iteration)
+		{
+			#pragma omp critical(cout)
+			std::cout << "Stage 2 " << omp_get_thread_num () << " / " << omp_get_num_threads() << 
+				": iteration " << iteration << " / " << iteration_count << std::endl;
+		}
+		
+		#pragma omp for
+		for (int iteration = 0; iteration < iteration_count; ++iteration)
+		{
+			#pragma omp critical(cout)
+			std::cout << "Stage 3 " << omp_get_thread_num () << " / " << omp_get_num_threads() << 
+				": iteration " << iteration << " / " << iteration_count << std::endl;
+		}
+	}
+	return 0;	
+}
